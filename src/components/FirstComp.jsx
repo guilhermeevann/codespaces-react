c59714297f0b478faaf82c011eb1efee979e5c76
guilhermeevann <139395847+guilhermeevann@users.noreply.github.com>
2024-10-22 13:42:01 +0000

    commit-guilherme

---
+++ src/components/FirstComp.jsx
@@ -0,0 +1,11 @@
+import "../estilos/FirstComp.css"
+
+function FirstComp() {
+  return (
+    <div className="container-test">
+      <h1>Meu primeiro componente</h1>
+    </div>
+  );
+}
+
+export default FirstComp;
